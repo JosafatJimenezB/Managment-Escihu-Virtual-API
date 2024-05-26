1b3d1e4e55b2b83398bc24b792c47ff58044abc8
Texhnolyze47 <ichigobankai136@gmail.com>
2024-05-26 10:10:44 -0600

    fix: error with the constructor

--- src/main/java/com/escihu/apiescihuvirtual/persistence/Entity/Student/Student.java
+++ src/main/java/com/escihu/apiescihuvirtual/persistence/Entity/Student/Student.java
@@ -93,7 +93,8 @@ public class Student {
     @ManyToMany(mappedBy = "students", fetch = FetchType.LAZY)
     private List<Course> courses;
 
-    public Student(Long id, String matricula, StatusStudent statusAlumno, User user, String nombre, String apellidoPaterno, String apellidoMaterno, Licenciatura licenciatura, String curp, String nacionalidad, SexoEnum sexo, String tipoSangre, EstadoCivilEnum estadoCivil, String telefono, String celular, String ingresoMensual, String institucionProcedencia, String institucionProcedenciaEstado, String institucionProcedenciaMunicipio, String correoPersonal, String correoEscolar, String nss, Address direccion, List<Course> courses) {
+
+    public Student(Long id, String matricula, StatusStudent statusAlumno, User user, String nombre, String apellidoPaterno, String apellidoMaterno, Licenciatura licenciatura, String curp, String nacionalidad, SexoEnum sexo, String tipoSangre, EstadoCivilEnum estadoCivil, String telefono, String celular, String ingresoMensual, String institucionProcedencia, String institucionProcedenciaEstado, String institucionProcedenciaMunicipio, String correoPersonal, String correoEscolar, Address direccion, String nss, List<Course> courses) {
         this.id = id;
         this.matricula = matricula;
         this.statusAlumno = statusAlumno;
@@ -115,10 +116,25 @@ public class Student {
         this.institucionProcedenciaMunicipio = institucionProcedenciaMunicipio;
         this.correoPersonal = correoPersonal;
         this.correoEscolar = correoEscolar;
-        this.nss = nss;
         this.direccion = direccion;
         this.nss = nss;
         this.courses = courses;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getNss() {
+        return nss;
+    }
+
+    public void setNss(String nss) {
+        this.nss = nss;
     }
 
     public Student() {
@@ -284,13 +300,6 @@ public class Student {
         this.correoEscolar = correoEscolar;
     }
 
-    public String getNss() {
-        return nss;
-    }
-
-    public void setNss(String nss) {
-        this.nss = nss;
-    }
 
     public Address getDireccion() {
         return direccion;
