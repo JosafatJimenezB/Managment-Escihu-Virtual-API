786aec02163587ef2f79f934a7687e8390d03c6a
Texhnolyze47 <ichigobankai136@gmail.com>
2024-05-25 21:37:42 -0600

    add: exception for email error

--- src/main/java/com/escihu/apiescihuvirtual/service/EmailService.java
+++ src/main/java/com/escihu/apiescihuvirtual/service/EmailService.java
@@ -2,7 +2,10 @@ package com.escihu.apiescihuvirtual.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender emailSender;
     private final Environment env;
 
@@ -17,6 +21,7 @@ public class EmailService {
         this.emailSender = emailSender;
         this.env = env;
     }
+    // TODO: Manejar excepciones en caso de se configutr stmp para gmail
     //TODO: documentar
     public void sendUserCredencials(String to, String username, String password) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
@@ -26,7 +31,13 @@ public class EmailService {
         helper.setSubject("Tus credenciales de acceso a EsciHu Virtual");
         helper.setText("Dear user,\n\nYour account has been created.\n\nUsername: " + username + "\nPassword: " + password + "\n\nBest regards,\nYour Team");
 
-        emailSender.send(message);
+        try {
+            emailSender.send(message);
+
+        } catch (MailException e) {
+            LOGGER.error("Error sending email to {}", to, e);
+            //TODO: Implementar un mensaje de error en caso de que no se pueda enviar el correo
+        }
 
     }
     //TODO: documentar
@@ -42,6 +53,11 @@ public class EmailService {
                 </div>
                 """.formatted(port,to), true);
 
-        emailSender.send(message);
+        try {
+            emailSender.send(message);
+        } catch (MailException e) {
+            LOGGER.error("Error sending email to {}", to, e);
+            //TODO: Implementar un mensaje de error en caso de que no se pueda enviar el correo
+        }
     }
 }
