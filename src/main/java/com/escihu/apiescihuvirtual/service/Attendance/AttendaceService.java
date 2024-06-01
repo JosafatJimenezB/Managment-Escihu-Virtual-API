a1acd9589cfb1b7b9188292a1fbba4a437847cb3
Texhnolyze47 <ichigobankai136@gmail.com>
2024-06-01 13:42:09 -0600

    docs: javadocs en AttendaceService

--- src/main/java/com/escihu/apiescihuvirtual/service/Attendance/AttendaceService.java
+++ src/main/java/com/escihu/apiescihuvirtual/service/Attendance/AttendaceService.java
@@ -8,12 +8,27 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface AttendaceService {
-
-    public Attendance register(AttendaceDtoRequest attendaceDtoRequest);
-
-    public Page<Attendance> attendancePagination(Pageable pageable);
-
-    public List<Attendance> listAll();
-
-    public Page<Attendance> attendaceByUserId(Long userId, Pageable pageable);
+    /**
+     * Registra la asistencia de un usuario
+     * @param attendaceDtoRequest objeto con los datos de la asistencia {@link AttendaceDtoRequest}
+     * @return la asistencia {@link Attendance}
+     */
+    Attendance register(AttendaceDtoRequest attendaceDtoRequest);
+    /**
+     * Obtiene la asistencia paginada
+     * @param pageable objeto con los datos de paginación {@link Pageable}
+     * @return lista de asistencias {@link Attendance}
+     */
+    Page<Attendance> attendancePagination(Pageable pageable);
+    /**
+     * Obtiene todas las asistencias
+     * @return lista de asistencias {@link Attendance}
+     */
+    List<Attendance> listAll();
+    /**
+     * Obtiene la asistencias paginadas por id
+     * @param userId id del usuario
+     * @return la asistencia {@link Attendance}
+     */
+    Page<Attendance> attendaceByUserId(Long userId, Pageable pageable);
 }
