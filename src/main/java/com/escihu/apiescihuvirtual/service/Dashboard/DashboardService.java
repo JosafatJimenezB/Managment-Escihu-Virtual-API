e47c94cb74e435bddbc50257bda6a0cfaaecdb13
Texhnolyze47 <ichigobankai136@gmail.com>
2024-06-01 08:56:35 -0600

    docs: javadocs en DashboardService

--- src/main/java/com/escihu/apiescihuvirtual/service/Dashboard/DashboardService.java
+++ src/main/java/com/escihu/apiescihuvirtual/service/Dashboard/DashboardService.java
@@ -7,13 +7,35 @@ import java.util.List;
 
 public interface DashboardService {
 
-    public int getTotalStudents();
+    /**
+     * Obtiene el total de estudiantes.
+     * @return el total de estudiantes
+     */
+    int getTotalStudents();
 
-    public int getTotalTeachers();
+    /**
+     * Obtiene el total de profesores.
+     * @return el total de profesores
+     */
+    int getTotalTeachers();
 
-    public int getTotalClassrooms();
+    /**
+     * Obtiene el total de aulas.
+     * @return el total de aulas
+     */
+    int getTotalClassrooms();
 
-    public List<Course> getCoursesByTeacher(String username);
+    /**
+     * Obtiene los cursos de un profesor.
+     * @param username el nombre de usuario del profesor
+     * @return la lista de cursos
+     */
+    List<Course> getCoursesByTeacher(String username);
 
-    public List<Subject> getSubjectsByTeacher(String username);
+    /**
+     * Obtiene las materias de un profesor.
+     * @param username el nombre de usuario del profesor
+     * @return la lista de materias
+     */
+    List<Subject> getSubjectsByTeacher(String username);
 }
