18fd1e8cf3bad3c806485a2efe019a0ecfa67ff6
Texhnolyze47 <ichigobankai136@gmail.com>
2024-06-01 08:42:29 -0600

    docs: javadocs en UserUtils

--- src/main/java/com/escihu/apiescihuvirtual/utils/UserUtils.java
+++ src/main/java/com/escihu/apiescihuvirtual/utils/UserUtils.java
@@ -6,9 +6,30 @@ import com.escihu.apiescihuvirtual.persistence.Repository.UserRepository;
 import java.security.SecureRandom;
 import java.util.Optional;
 
+/**
+ * Clase con métodos útiles para la entidad {@link User}. Proporciona dos métodos estáticos como
+ * generadores de correo electrónico y contraseñas aleatorias.
+ * <ul>
+ *     <li>{@link #generateEmail(String, String, UserRepository)} Genera un correo electrónico a partir del nombre y apellido paterno del usuario</li>
+ *     <li>{@link #generateRandomPassword()} Genera una contraseña aleatoria</li>
+ * </ul>
+ *
+ */
 public class UserUtils {
 
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+    private static final int PASSWORD_LENGTH = 12; // Longitud deseada de la contraseña
 
+
+    /**
+     * Genera un correo electrónico a partir del nombre y apellido paterno del usuario
+     * en caso de que el correo ya exista, se le agrega un número al final
+     *
+     * @param nombre El nombre del usuario
+     * @param apellidoPaterno El apellido paterno del usuario
+     * @param userRepository Repositorio de usuarios
+     * @return Un string con el correo electrónico generado
+     */
     public static String generateEmail(String nombre, String apellidoPaterno, UserRepository userRepository) {
         String correo = "";
         correo += nombre.toLowerCase() + "." + apellidoPaterno.toLowerCase() + "@escihu.com";
@@ -23,9 +44,11 @@ public class UserUtils {
         return correo;
     }
 
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
-    private static final int PASSWORD_LENGTH = 12; // Longitud deseada de la contraseña
-
+    /**
+     * Genera una contraseña aleatoria
+     *
+     * @return un string con la contraseña aleatoria
+     */
     public static String generateRandomPassword() {
         SecureRandom random = new SecureRandom();
         StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
