dc776d558bed53dda6764cb71385c71e4a44153d
Texhnolyze47 <ichigobankai136@gmail.com>
2024-06-02 00:02:30 -0600

    migrations add temp-data

---
+++ src/main/resources/db/migration/V6__insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO licenciatura(id, name,code) VALUES (1, 'Licenciatura en comunicacion',01);
+INSERT INTO licenciatura(id, name,code) VALUES (2, 'Licenciatura en administracion y gestions de negociones emprededores',02);
+INSERT INTO licenciatura(id, name,code) VALUES (3, 'Licenciatura en ingenieria en administracion industrial',03);
+INSERT INTO licenciatura(id, name,code) VALUES (4, 'Licenciatura en mercadotecnia estrategica',04);
+INSERT INTO licenciatura(id, name,code) VALUES (5, 'Licenciatura en Diseño grafica',05);
+INSERT INTO licenciatura(id, name,code) VALUES (6, 'Licenciatura en ingenieria en sistemas computacionales',06);
